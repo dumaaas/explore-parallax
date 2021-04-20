92726eb37d267d40e663384d4631f44ae11a7951
dumaaas <markodumnic8@gmail.com>
2021-04-21 01:18:38 +0200

    Finished scene

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
 let controller = new ScrollMagic.Controller();
 let timeline = new TimelineMax();
 
-timeline
-    .to('.rock', 3, {y: -300})
-    .to('.girl', 3, {y: -200}, '-=3')
-    .to('.bg1', 3, {y: 50}, "-=3");
+timeline //setting timeline -> how elements should behave
+    .to('.rock', 3, {
+        y: -300
+    })
+    .to('.girl', 3, {
+        y: -200
+    }, '-=3')
+    .fromTo('.bg1', 3, {
+        y: -50
+    }, {
+        y: 0
+    }, "-=3")
+    .to('.content', 3, {
+        top: '0%'
+    }, "-=3")
+    .fromTo(".content-images", {
+        opacity: 0
+    }, {
+        opacity: 1,
+        duration: 3
+    })
+    .fromTo(".text", {
+        x: -1600
+    }, {
+        x: 0,
+        duration: 3
+    }, "-=3");
+
+let scene = new ScrollMagic.Scene({
+        triggerElement: 'section', //trigger scene when reach section
+        duration: '100%', //duration 
+        triggerHook: 0, //when to trigger scene (0 -> instanst on scrolling, 1 -> when we reach end of the section)
+    })
+    .setTween(timeline)
+    .setPin("section")
+    .addTo(controller);
